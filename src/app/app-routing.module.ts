327f8316b4c721c3cddf00b47776ac762cd74c9d
Smit Patel <smitpatel316@gmail.com>
2020-06-24 14:54:40 -0400

    about and experience

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ExperienceComponent } from './experience/experience.component';
+import { AcademicsComponent } from './academics/academics.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ResumeComponent } from './resume/resume.component';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'academics', component: AcademicsComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'resume', component: ResumeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: '/about', pathMatch: 'full' },
+  { path: '**', redirectTo: '/about', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { SkillsComponent } from './skills/skills.component';
+import { AcademicsComponent } from './academics/academics.component';
+import { ResumeComponent } from './resume/resume.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MglTimelineModule } from 'angular-mgl-timeline';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    AcademicsComponent,
+    ResumeComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule,
+    MglTimelineModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
