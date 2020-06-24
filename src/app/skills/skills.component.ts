3b7c2b52fead3b683d23ea7c6b1bebe201e5f952
Smit Patel <smitpatel316@gmail.com>
2020-06-24 16:31:35 -0400

    skills

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss']
+  styleUrls: ['./skills.component.scss'],
 })
 export class SkillsComponent implements OnInit {
+  names = [
+    'AWS S3',
+    'Android',
+    'Angular',
+    'Bash',
+    'C',
+    'Flask',
+    'Git',
+    'Google Cloud Platform',
+    'HTML',
+    'Hadoop',
+    'Haskell',
+    'Ionic',
+    'Java',
+    'JavaScript',
+    'Jenkins',
+    'Kubernetes',
+    'Linux',
+    'MongoDB',
+    'MySQL',
+    'NodeJS',
+    'Openshift',
+    'Python',
+    'React',
+    'Scala',
+    'Spring',
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  getImagePath(name: string) {
+    return `../../assets/skills/${name}.svg`;
   }
-
 }
